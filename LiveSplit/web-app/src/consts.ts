3221ca6244d50cbf26210dc171afc7e25c9e3164
ellizio <profillive@mail.ru>
2024-10-30 18:35:59 +0300

    Changed urls

--- LiveSplit/web-app/src/consts.ts
+++ LiveSplit/web-app/src/consts.ts
@@ -9,12 +9,12 @@ export const SPLITS_FILENAME = 'Gothic3-LiveSplit.lss'
 // Links
 export const MANUAL_URL = 'https://github.com/ellizio/Gothic3-LiveSplit/blob/master/LiveSplit/README.md'
 
-export const QUESTS_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/quests'
-export const QUESTS_EN_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/quests.en'
+export const QUESTS_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/quests'
+export const QUESTS_EN_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/quests.en'
 
-export const SKILLS_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/skills'
-export const SKILLS_EN_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/skills.en'
+export const SKILLS_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/skills'
+export const SKILLS_EN_SOURCE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/skills.en'
 
-export const SCRIPT_TEMPLATE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/script-template'
-export const HELPER_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/Gothic3-LiveSplit.dll'
-export const SPLITS_TEMPLATE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/generator/sources/splits-template'
+export const SCRIPT_TEMPLATE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/script-template'
+export const HELPER_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/Gothic3-LiveSplit.dll'
+export const SPLITS_TEMPLATE_URL = 'https://raw.githubusercontent.com/ellizio/Gothic3-LiveSplit/refs/heads/master/sources/splits-template'
